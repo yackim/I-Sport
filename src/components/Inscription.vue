8c25b5ae0a466c8f76e0fd89df58012db8a5b131
Ashlalunah <helihasinalalaina03@gmail.com>
2021-12-13 12:51:50 +0100

    template inscription 2

--- src/components/Inscription.vue
+++ src/components/Inscription.vue
@@ -1,149 +1,67 @@
 <template>
+<html lang="en">
+  <head>
+  	<title>Login 09</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<body>
-  <div class="elevation-12" >
-   <form>
+		
+	<link rel="stylesheet" href="src/assets/login19/css/style.css">
 
-              <h1 class="text-center display=2 teal==text text==accent-3">
-                              Inscription Etudiant</h1>
-    
-    <div class="ve">
-    <BR></BR><br><br>
-    <P class="pere">Salut les Amis!</P>
-    <p class="pere"> Créer un nouveau Compte</p>
-    <br>
-    <button rounded  class="inscrire"><i class="fa fa-home"></i></button>
-  </div>
-  
-   </form>
-  </div>
-      <table>
-      <tr>
-          <td><label class="soratra" for=""><i class="fa fa-user"></i></label>
-          <input type="submit" class="boite" value="Etudiant"></td>  
-      </tr> 
-      <br>
-       <tr>
-          <td><label class="soratra" for=""><i class="fa fa-envelope"></i></label>
-          <input type="text" class="boite" placeholder="veuillez mettre votre Email"></td>  
-      </tr><br>
-      <tr>
-          <td><label for=""><i class="fa fa-university"></i></label>
-          <input class="boite" type="password" placeholder="veuillez mettre votre Classe"></td>
-      </tr>  <br>
-       <tr>
-          <td><label class="soratra" for=""><i class="fa fa-qrcode"></i></label>
-          <input type="text" class="boite" placeholder="veuillez mettre votre Matricule"></td>  
-      </tr><br>
-      <tr>
-          <td><label for=""><i class="fa fa-unlock-alt"></i></label>
-          <input class="boite" type="password" placeholder="veuillez mettre votre Mot de passe"></td>
-      </tr><br>
-      <tr>
-          <td><p class="pty">confirmer votre Mot de Passe</p>
-          <input class="boite1" type="password" placeholder="veuillez confirmervotre Mot de passe"></td>
-      </tr>   
-      <br>
-      <tr>   
-         <td colspan="2" class="re" text-align="center">
-          
-          <br> <button rounded >ENREGISTRER</button></td>
-      </tr>
-  </table>
-  
+	</head>
+	<body>
+	<section class="ftco-section">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-md-6 text-center mb-5">
+					<h2 class="heading-section"></h2>
+				</div>
+			</div>
+			<div class="row justify-content-center">
+				<div class="col-md-6 col-lg-4">
+					<div class="login-wrap py-5">
+		      	<div class="img d-flex align-items-center justify-content-center" style="background-image: url(src/assets/login19/images/bg.jpg);"></div>
+		      	<h3 class="text-center mb-0">Inscription Etudiant</h3>
+		      	<p class="text-center">Inscrivez-vous içi en entrant les informations suivants</p>
+						<form action="#" class="login-form">
+		      		<div class="form-group">
+		      			<div class="icon d-flex align-items-center justify-content-center"><span class="fa fa-user"></span></div>
+		      			<input type="text" class="form-control" placeholder="Nom d'utilisateur" required>
+		      		</div>
+					  <div class="form-group">
+		      			<div class="icon d-flex align-items-center justify-content-center"><span class="fa fa-envelope"></span></div>
+		      			<input type="text" class="form-control" placeholder="E-Mail" required>
+		      		</div>
+					  <div class="form-group">
+		      			<div class="icon d-flex align-items-center justify-content-center"><span class="fa fa-university"></span></div>
+		      			<input type="text" class="form-control" placeholder="Votre Classe" required>
+		      		</div>
+					  <div class="form-group">
+		      			<div class="icon d-flex align-items-center justify-content-center"><span class="fa fa-qrcode"></span></div>
+		      			<input type="text" class="form-control" placeholder="Votre Matricule" required>
+		      		</div>
+	            <div class="form-group">
+	            	<div class="icon d-flex align-items-center justify-content-center"><span class="fa fa-lock"></span></div>
+	              <input type="password" class="form-control" placeholder="Mots de passe" required>
+	            </div>
+	            
+	            <div class="form-group">
+	            	<button type="submit" class="btn form-control btn-primary rounded submit px-3">SAUVEGARDER</button>
+	            </div>
+	          </form>
+	          <div class="w-100 text-center mt-4 text">
+	          	
+		          <a href="#">RETOUR</a>
+	          </div>
+	        </div>
+				</div>
+			</div>
+		</div>
+	</section>
 
-</body>
+	
+	</body>
+</html>
+
+
 </template>
- 
-<style scoped>
-v-container{
-  background-color: brown;
-}
-.elevation-12{
-  margin-top: 5%;
-  margin-left: 15%;
- display: flex;
-  flex-direction: column;
- height: 500px;
- width: 713px;
- font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-  border: 1px solid rgb(43, 184, 30);
-     background-image: url(../assets/fond.jpg);
-}
-table{
-    margin-top:-441px;
-    margin-left:531px;
-    background-color: snow;
-}
-
-button{
-  background-color: rgb(238, 224, 25);
-  border-radius: 30px;
-  border: 1px solid rgb(43, 184, 30);
-  background-color: rgb(238, 224, 25);
-  font-size: 12px;
-  font-weight: bold;
-  padding: 12px 45px;
-  text-transform: uppercase;
-      MARGIN-LEFT: 55px;
-    margin-top: -69px;
-  }
-.re{
-  text-align: center;
-}
-.rez{
-      margin-left: 397px;
-}
-.boite{
-  width: 250px;
-  border: 2px solid rgb(236, 224, 25);
-  border-radius: 12px;
-}
-.pty{
-  margin-left:23px;
-}
-.boite1{
-  margin-left:17px;
-   width: 250px;
-  border: 2px solid rgb(236, 224, 25);
-  border-radius: 12px;
-}
-h1{
-  margin-left: 235px;
-}
-img{
-     width: 216px;
-    height: 166px;
-    margin-left: 165px;
-    margin-top: 15px;
-}
-.soratra{
-   font-family: Arial, Helvetica, sans-serif;
-   font-size: 19px;
-  font-weight: bold;
-}
-
-
-.pere{
-  font-family: Arial, Helvetica, sans-serif;
-   font-size: 19px;
-  font-weight: bold;
-      margin-left: 42px;
-}
-.inscrire{
-    margin-top: 12px;
-    margin-left: 59px;
-}
-.soratra1{
-      width: 39px;
-    height: 28px;
-}
-.ree{
-  margin-left: 212px;
-}
-body{
-    background-image: url(../assets/SE.jpg);
-    width: 100%;
-    height: 100%;
-}
-</style>
--- src/components/login.vue
+++ src/components/login.vue
@@ -1,138 +1,59 @@
 <template>
-<body>
-  
-  <div class="elevation-12" >
-   <form>
+<html lang="en">
+  <head>
+  	<title>Login 09</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-              <h1 class="margin-left=72px display=2 teal==text text==accent-3">
-                              Authentification</h1>
-    <img src="../assets/LK.png" class="rez" alt="">
-      <table>
-      <tr>
-          <td><label class="soratra" for=""><i class="fa fa-envelope"></i></label></td>
-          <td><input type="text" class="boite" placeholder="veuillez mettre votre Email"></td>  
-      </tr>
-      <br>
-      <tr>
-          <td><label for=""><i class="fa fa-unlock-alt"></i></label></td>
-          <td><input class="boite" type="password" placeholder="veuillez mettre votre Mot de passe"></td>
-      </tr>  
-      
-      <tr>   
-         <td colspan="2" class="re" text-align="center">
-           <p><a href="" class="ree">Mot de Passe oublié</a></p> 
-           </td>
-      </tr>
-  </table>
-  <button rounded class="boutton1" >CONNECTER</button>
-  <div class="ve">
-    <BR></BR><br><br>
-    <P class="pere">Salut les Amis!</P>
-    <p class="pere"> Créer un nouveau Compte</p>
-    <br>
-    <button rounded outlined="" dark @click="step++" class="inscrire">s'INSCRIRE</button>
-  </div>
-  
-   </form>
-  </div>
+		
+	<link rel="stylesheet" href="src/assets/login19/css/style.css">
 
-</body>
+	</head>
+	<body>
+	<section class="ftco-section">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-md-6 text-center mb-5">
+					<h2 class="heading-section"></h2>
+				</div>
+			</div>
+			<div class="row justify-content-center">
+				<div class="col-md-6 col-lg-4">
+					<div class="login-wrap py-5">
+		      	<div class="img d-flex align-items-center justify-content-center" style="background-image: url(src/assets/login19/images/bg.jpg);"></div>
+		      	<h3 class="text-center mb-0">Bienvenue</h3>
+		      	<p class="text-center">Pour accèder à l'Application veuillez renseigner les champs</p>
+						<form action="#" class="login-form">
+		      		<div class="form-group">
+		      			<div class="icon d-flex align-items-center justify-content-center"><span class="fa fa-user"></span></div>
+		      			<input type="text" class="form-control" placeholder="Nom d'utilisateur" required>
+		      		</div>
+	            <div class="form-group">
+	            	<div class="icon d-flex align-items-center justify-content-center"><span class="fa fa-lock"></span></div>
+	              <input type="password" class="form-control" placeholder="Mots de passe" required>
+	            </div>
+	            <div class="form-group d-md-flex">
+								<div class="w-100 text-md-right">
+									<a href="#">Mots de passe oublier</a>
+								</div>
+	            </div>
+	            <div class="form-group">
+	            	<button type="submit" class="btn form-control btn-primary rounded submit px-3">Se connecter</button>
+	            </div>
+	          </form>
+	          <div class="w-100 text-center mt-4 text">
+	          	<p class="mb-0">vous n'avez pas de compte?</p>
+		          <a href="#">S'inscrire</a>
+	          </div>
+	        </div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	
+	</body>
+</html>
+
+
 </template>
-
-<style scoped>
-v-container{
-  background-color: brown;
-}
-.elevation-12{
-  margin-top: 5%;
-  margin-left: 15%;
- display: flex;
-  flex-direction: column;
- height: 500px;
- width: 713px;
- font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-  border: 1px solid rgb(43, 184, 30);
-     background-image: url(../assets/fond.jpg);
-}
-table{
-    margin-top: 73px;
-    margin-left: 337px;
-}
-button{
-    background-color: rgb(238, 224, 25);
-  border-radius: 30px;
-  border: 1px solid rgb(43, 184, 30);
-  background-color: rgb(238, 224, 25);
-  font-size: 12px;
-  font-weight: bold;
-  padding: 12px 45px;
-  text-transform: uppercase;
-}
-.boutton1{
-  background-color: rgb(238, 224, 25);
-  border-radius: 20px;
-  border: 1px solid rgb(43, 184, 30);
-  background-color: rgb(238, 224, 25);
-  font-size: 12px;
-  font-weight: bold;
-  padding: 12px 45px;
-  text-transform: uppercase;
-  MARGIN-LEFT: 421px;
-  margin-top: -60px;
-  }
-.re{
-  text-align: center;
-}
-.rez{
-    margin-left: 380px;
-}
-.boite{
-  width: 250px;
-  border: 2px solid rgb(236, 224, 25);
-}
-.ve {
-     margin-right: 0px;
-    width: 257px;
-    height: 500px;
-    margin-top: -66%;
-    margin-left: 0%;
-    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-}
-img{
-     width: 216px;
-    height: 166px;
-    margin-left: 165px;
-    margin-top: 15px;
-}
-.soratra{
-   font-family: Arial, Helvetica, sans-serif;
-   font-size: 19px;
-  font-weight: bold;
-}
-
-.pere{
-  font-family: Arial, Helvetica, sans-serif;
-   font-size: 19px;
-  font-weight: bold;
-      margin-left: 42px;
-}
-.inscrire{
-    margin-top: 12px;
-    margin-left: 59px;
-}
-.soratra1{
-    width: 56px;
-    height: 51px;
-}
-.ree{
-  margin-left: 212px;
-}
-h1{
-  margin-left: 365px;
-}
-body{
-    background-image: url(../assets/SE.jpg);
-    width: 100%;
-    height: 100%;
-}
-</style> 
